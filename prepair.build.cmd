30052fbfac95d53d4ecc9376141480fe4a35f5d4
Ilya Tkachenko <ilyamsu@gmail.com>
2020-11-20 17:55:13 +0300

    Add release bundle script

---
+++ prepair.build.cmd
@@ -0,0 +1,13 @@
+@mkdir ReleaseBundle\lib
+@mkdir ReleaseBundle\include
+
+@copy "!bin\HookEngine\Debug\x64\HookEngine-x64-md-d.lib" ReleaseBundle\lib\
+@copy "!bin\HookEngine\Debug\x86\HookEngine-x86-md-d.lib" ReleaseBundle\lib\
+@copy "!bin\HookEngine\Debug-MT\x64\HookEngine-x64-mt-d.lib" ReleaseBundle\lib\
+@copy "!bin\HookEngine\Debug-MT\x86\HookEngine-x86-mt-d.lib" ReleaseBundle\lib\
+@copy "!bin\HookEngine\Release\x64\HookEngine-x64-md.lib" ReleaseBundle\lib\
+@copy "!bin\HookEngine\Release\x86\HookEngine-x86-md.lib" ReleaseBundle\lib\
+@copy "!bin\HookEngine\Release-MT\x64\HookEngine-x64-mt.lib" ReleaseBundle\lib\
+@copy "!bin\HookEngine\Release-MT\x86\HookEngine-x86-mt.lib" ReleaseBundle\lib\
+
+@xcopy /s "HookEngine\include\HookEngine\*.h" "ReleaseBundle\include\HookEngine\\"
